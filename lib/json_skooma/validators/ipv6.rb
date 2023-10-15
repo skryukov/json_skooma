59aa0567eb45bab3b54f486ff5cf667824015c4c
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2023-10-15 17:39:48 +0300

    Fix `ipv4` and `ipv6` validations for Ruby < 3.1

--- lib/json_skooma/validators/ipv6.rb
+++ lib/json_skooma/validators/ipv6.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-require "ipaddr"
-
 module JSONSkooma
   module Validators
     class Ipv6 < Base
+      H16 = /\h{1,4}/
+      LS32 = /(?:#{H16}:#{H16})|#{Ipv4::IPV4_ADDRESS}/
+      IPV6_ADDRESS = /(?:#{H16}:){6}#{LS32}|::(?:#{H16}:){5}#{LS32}|(?:#{H16})?::(?:#{H16}:){4}#{LS32}|(?:(?:#{H16}:){0,1}#{H16})?::(?:#{H16}:){3}#{LS32}|(?:(?:#{H16}:){0,2}#{H16})?::(?:#{H16}:){2}#{LS32}|(?:(?:#{H16}:){0,3}#{H16})?::(?:#{H16}:){1}#{LS32}|(?:(?:#{H16}:){0,4}#{H16})?::#{LS32}|(?:(?:#{H16}:){0,5}#{H16})?::#{H16}|(?:(?:#{H16}:){0,6}#{H16})?::/.freeze
+
+      REGEXP = /\A#{IPV6_ADDRESS}\z/
+
       def call(data)
-        ip = IPAddr.new "#{data.value}/64"
-        raise FormatError, "must be a valid IPv6 address" unless ip.ipv6? && ip.zone_id.nil?
-      rescue IPAddr::Error => e
-        raise FormatError, e.message
+        match = REGEXP.match(data)
+        raise FormatError, "must be a valid IPv6 address" if match.nil?
       end
     end
   end
