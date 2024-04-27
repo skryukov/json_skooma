54123e159a6fa5b1b987f8f1417e31418bb6a3db
Alexander Marychev <killondark@gmail.com>
2024-04-27 13:44:29 +0300

    Revert auto-resolve part logic

--- lib/json_skooma/keywords/core/ref.rb
+++ lib/json_skooma/keywords/core/ref.rb
@@ -7,29 +7,12 @@ module JSONSkooma
         self.key = "$ref"
 
         def resolve
-          add_remote_source if need_to_adding?
           @ref_schema = parent_schema.resolve_ref(json)
         end
 
         def evaluate(instance, result)
           @ref_schema.evaluate(instance, result)
           result.ref_schema = @ref_schema
-        end
-
-        def add_remote_source
-          parent_schema.registry.add_source(
-            pathname.dirname.to_s + "/",
-            JSONSkooma::Sources::Remote.new(json)
-          )
-        end
-
-        def need_to_adding?
-          json.start_with?("http") &&
-            parent_schema.registry.instance_variable_get("@uri_sources").keys.none?{ |k| json.include?(k) }
-        end
-
-        def pathname
-          Pathname.new(json)
         end
       end
     end
